530895d2449ec59d7b39106e25c11d24f446ebdc
chenbauer <chen@funtomic.com>
2013-10-08 20:32:38 +0300

    Remove click_data cookie after reading it

--- lib/game_stats/helper.rb
+++ lib/game_stats/helper.rb
@@ -5,7 +5,8 @@ module GameStats
 
     def collect_click_data
       if cookies["click_data"].present?
-        click_data = cookies["click_data"].split('|')
+        click_data = cookies['click_data'].split('|')
+        cookies.delete 'click_data'
         game_id = click_data[0]
         similar_game_id = click_data[1]
         GameStat.record_click game_id, similar_game_id
